8d2bf668c5edaab9f077ba1e761cdbe33ddaeab1
Mat Munn <mat@matmunn.me>
2021-06-30 07:16:44 +1000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let data = ''
+</script>
+
+<div>
+  <textarea
+    bind:value={data}
+    cols={80}
+    rows={8}
+  />
+  <br />
+  {#if data}
+    <img src={`data:;base64,${data}`} />
+  {/if}
+</div>
